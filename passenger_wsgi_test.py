4beec687d8ad84ff5cea0c4117876a0ba7f0e569
dtiberio <dtiberio@users.noreply.github.com>
2023-09-02 22:47:26 +0100

    Update passenger_wsgi_test.py
    added comment

--- passenger_wsgi_test.py
+++ passenger_wsgi_test.py
@@ -1,5 +1,6 @@
 # passenger_wsgi.py
 # testing, place this file in the Parent folder of your Document Root
+# Alternatively, if you've set the Apache Directive:PassengerAppRoot, then place the file there
 
 def application(environ, start_response):
    response_headers = [('Content-type','text/plain')]
