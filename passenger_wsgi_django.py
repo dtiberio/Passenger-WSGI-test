6dec00c3ec0d83a70d3b419b1542d4303d72ca7e
dtiberio <dtiberio@users.noreply.github.com>
2023-09-02 22:54:58 +0100

    Update passenger_wsgi_django.py
    added comment

--- passenger_wsgi_django.py
+++ passenger_wsgi_django.py
@@ -1,13 +1,14 @@
 # passenger_wsgi.py
 
 # by default, PassengerAppRoot is the Parent of vhost DocumentRoot, and this file needs to be stored there
+# if you've set the Apache Directive: PassengerAppRoot, then place this file there
 
 import os
 
 # Set the DJANGO_SETTINGS_MODULE environment variable
 # Replace 'path.your_project' with your project's actual name, relative to this file
 # Update ALLOWED_HOSTS in settings.py
-# if needed, touch ./tmp/restart.txt
+# if needed, touch ./tmp/restart.txt, this is the Passenger restart file
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'path.your_project.settings' 
 
