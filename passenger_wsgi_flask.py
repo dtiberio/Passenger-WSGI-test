de81b1a55af8278ea96c7a9efb338910700fc4f0
dtiberio <dtiberio@users.noreply.github.com>
2023-09-02 22:50:46 +0100

    Update passenger_wsgi_flask.py
    added additional comment

--- passenger_wsgi_flask.py
+++ passenger_wsgi_flask.py
@@ -1,2 +1,6 @@
 # passenger_wsgi.py
+# assumption: the Flask app.py is located on the same folder as this file
+# it that's not the case, then update the location: 
+# from path.app import MyApp as application
+
 from app import MyApp as application
