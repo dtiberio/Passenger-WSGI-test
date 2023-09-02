84e78196c11b8f030fd956cecbea67849a5fd3b6
dtiberio <dtiberio@users.noreply.github.com>
2023-09-02 22:52:38 +0100

    Update passenger_wsgi_flask.py
    added url to Flask demo app

--- passenger_wsgi_flask.py
+++ passenger_wsgi_flask.py
@@ -2,5 +2,6 @@
 # assumption: the Flask app.py is located on the same folder as this file
 # it that's not the case, then update the location: 
 # from path.app import MyApp as application
+# pull a Flask demo app from here: https://github.com/phusion/passenger-python-flask-demo
 
 from app import MyApp as application
